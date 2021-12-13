ebdd9351d97cf1f5d82695494958675c1d1d2cf3
rimonspi12 <rimonmpi@gmail.com>
2021-12-13 08:03:35 +0600

    banner is added

--- src/Pages/Home/AppointmentBanner/AppointmentBanner.js
+++ src/Pages/Home/AppointmentBanner/AppointmentBanner.js
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import doctor from '../../../images/doctor.png';
 import bg from '../../../images/appointment-bg.png';
-import Button from '@mui/material/Button';
+import { Button, Container } from '@mui/material';
 
 
 const appointmentBanner = {
@@ -17,27 +17,29 @@ const appointmentBanner = {
 const AppointmentBanner = () => {
     return (
         <Box style={appointmentBanner} sx={{ flexGrow: 1 }}>
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                    <img
-                        style={{ width: 400, marginTop: '-110px' }}
-                        src={doctor} alt="" />
+           <Container>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={6}>
+                        <img
+                            style={{ width: 400, marginTop: '-110px' }}
+                            src={doctor} alt="" />
+                    </Grid>
+                    <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'flex-start', textAlign: 'left', alignItems: 'start' }}>
+                        <Box>
+                            <Typography variant="h6" sx={{ mb: 5 }} style={{ color: '#8CE6E8' }}>
+                                Appointment
+                            </Typography>
+                            <Typography variant="h4" style={{ color: 'white' }}>
+                                Make an Appointment Today
+                            </Typography>
+                            <Typography variant="h6" sx={{ my: 5 }} style={{ color: 'white', fontSize: 14, fontWeight: 400 }}>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti vero laudantium inventore facilis aperiam, quia dolorum asperiores unde aliquid soluta.
+                            </Typography>
+                            <Button variant="contained" style={{ backgroundColor: '#8CE6E8' }}>Learn More</Button>
+                        </Box>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6} sx={{ display: 'flex', justifyContent: 'flex-start', textAlign: 'left', alignItems: 'center' }}>
-                    <Box>
-                        <Typography variant="h6" sx={{mb:5}} style={{ color: '#8CE6E8' }}>
-                            Appointment
-                        </Typography>
-                        <Typography variant="h4" style={{ color: 'white' }}>
-                            Make an Appointment Today
-                        </Typography>
-                        <Typography variant="h6" sx={{ my: 5 }} style={{ color: 'white', fontSize: 14, fontWeight: 400 }}>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti vero laudantium inventore facilis aperiam, quia dolorum asperiores unde aliquid soluta.
-                        </Typography>
-                        <Button variant="contained" style={{ backgroundColor:'#8CE6E8'}}>Learn More</Button>
-                    </Box>
-                </Grid>
-            </Grid>
+           </Container>
         </Box>
     );
 };
---
+++ src/Pages/Home/Banner/Banner.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import chair from '../../../images/chair.png';
+import bg from '../../../images/bg.png';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import { Button, Container } from '@mui/material';
+import { Box } from '@mui/system';
+
+const bannerBackground = {
+    background:`url(${bg})`,
+   
+}
+
+const verticalCenter = {
+    display:'flex',
+    alignItems:'center',
+    height: 400,
+    // border:'1px solid red'
+}
+
+const Banner = () => {
+    return (
+        <div>
+            <Container style={bannerBackground} sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item style={{ ...verticalCenter,textAlign: 'left' }} xs={12} md={6}>
+                       <Box>
+                            <Typography variant="h3">
+                                Your New Smile <br /> Starts Here
+                            </Typography>
+                            <Typography variant="h6" sx={{ fontSize: 13, color: 'gray', fontWeight: 400 }}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit inventore, doloribus voluptatibus vel similique assumenda.
+                            </Typography>
+                            <Button variant="contained" style={{ backgroundColor: '#8CE6E8' }}>GET Appointment</Button>
+                       </Box>
+                    </Grid>
+                    <Grid item xs={12} md={6} style={verticalCenter} >
+                        <img style={{ width: '350px' }} src={chair} alt="" />
+                    </Grid>
+                </Grid>
+            </Container>
+        </div>
+    );
+};
+
+export default Banner;
