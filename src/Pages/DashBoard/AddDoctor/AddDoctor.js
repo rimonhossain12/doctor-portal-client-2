3b57a656f1abf77b4731a0b7c74b2bcc297e9d7e
rimonspi12 <rimonmpi@gmail.com>
2022-01-09 23:42:57 +0600

    added doctors image

--- src/Pages/DashBoard/AddDoctor/AddDoctor.js
+++ src/Pages/DashBoard/AddDoctor/AddDoctor.js
@@ -4,15 +4,36 @@ import React, { useState } from 'react';
 const AddDoctor = () => {
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
+    const [image, setImage] = useState(null);
 
-    // const handleName = e => {
-    //     setName(e.target.value);
-    // }
+    const handleSubmit = e => {
+        e.preventDefault();
+        if (!image) {
+            return;
+        }
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('email', email);
+        formData.append('image', image);
+        fetch('http://localhost:5000/doctors', {
+            method: 'POST',
+            body: formData
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId){
+                    console.log('doctor added successfully!');
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }
 
     return (
         <div>
             <h2>Add a Doctor</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <TextField
                     sx={{ width: '50%' }}
                     label="Name"
@@ -30,7 +51,9 @@ const AddDoctor = () => {
                 <br />
                 <Input
                     accept="image/*"
-                    type="file" />
+                    type="file"
+                    onChange={e => setImage(e.target.files[0])}
+                />
                 <br />
                 <Button variant="contained" type='submit'>
                     Add Doctor
